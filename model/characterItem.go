38e9d943b31a7b3737bd1df5c44afdf7c0df219f
Telpegaraf <telpegaraf@gmail.com>
2024-10-15 14:52:48 +0400

    updated struct for charactertem

--- model/characterItem.go
+++ model/characterItem.go
@@ -9,3 +9,15 @@ type CharacterItem struct {
 	Character Character `gorm:"foreignKey:CharacterId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	Item      Item      `gorm:"foreignKey:ItemId;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
+
+type CreateCharacterItem struct {
+	CharacterID uint `json:"character_id" query:"character_id" binding:"required" form:"character_id"`
+	ItemID      uint `json:"item_id" query:"item_id" binding:"required" form:"item_id"`
+	Quantity    uint `json:"quantity" query:"quantity" form:"quantity"`
+}
+
+type UpdateCharacterItem struct {
+	CharacterID uint `json:"character_id" query:"character_id" form:"character_id"`
+	ItemID      uint `json:"item_id" query:"item_id" form:"item_id"`
+	Quantity    uint `json:"quantity" query:"quantity" binding:"required" form:"quantity"`
+}
