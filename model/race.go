ea8e9c14fbfd0ff51cc3308198e25313c52601d9
Telpegaraf <telpegaraf@gmail.com>
2024-10-16 19:52:53 +0400

    added race struct

--- model/race.go
+++ model/race.go
@@ -21,3 +21,24 @@ type RaceCreate struct {
 	AbilityBoost string `json:"ability_boost" query:"ability_boost" form:"ability_boost"`
 	Language     string `json:"language" query:"language" form:"language"`
 }
+
+type RaceUpdate struct {
+	Name         string `json:"name" query:"name" form:"name"`
+	Description  string `json:"description" query:"description" form:"description"`
+	HitPoints    int8   `json:"hit_points" query:"hit_points" form:"hit_points"`
+	Size         string `json:"size" query:"size" form:"size"`
+	Speed        int8   `json:"speed" query:"speed" form:"speed"`
+	AbilityBoost string `json:"ability_boost" query:"ability_boost" form:"ability_boost"`
+	Language     string `json:"language" query:"language" form:"language"`
+}
+
+type RaceExternal struct {
+	ID           uint   `json:"id" query:"id" form:"id"`
+	Name         string `json:"name" query:"name" form:"name"`
+	Description  string `json:"description" query:"description" form:"description"`
+	HitPoints    int8   `json:"hit_points" query:"hit_points" form:"hit_points"`
+	Size         string `json:"size" query:"size" form:"size"`
+	Speed        int8   `json:"speed" query:"speed" form:"speed"`
+	AbilityBoost string `json:"ability_boost" query:"ability_boost" form:"ability_boost"`
+	Language     string `json:"language" query:"language" form:"language"`
+}
