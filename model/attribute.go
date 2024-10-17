f9e760aee8d1cb15426a537aa79448e46a5a8692
Telpegaraf <telpegaraf@gmail.com>
2024-10-17 10:46:05 +0400

    added structs for attribute

--- model/attribute.go
+++ model/attribute.go
@@ -1,12 +1,42 @@
 package model
 
 type Attribute struct {
-	ID           uint `gorm:"primary_key;AUTO_INCREMENT"`
-	Strength     int8 `gorm:"default:10"`
-	Dexterity    int8 `gorm:"default:10"`
-	Constitution int8 `gorm:"default:10"`
-	Intelligence int8 `gorm:"default:10"`
-	Wisdom       int8 `gorm:"default:10"`
-	Charisma     int8 `gorm:"default:10"`
-	CharacterID  uint `gorm:"unique"`
+	ID           uint  `gorm:"primary_key;AUTO_INCREMENT"`
+	Strength     uint8 `gorm:"default:10;not null;"`
+	Dexterity    uint8 `gorm:"default:10;not null;"`
+	Constitution uint8 `gorm:"default:10;not null"`
+	Intelligence uint8 `gorm:"default:10;not null"`
+	Wisdom       uint8 `gorm:"default:10;not null"`
+	Charisma     uint8 `gorm:"default:10;not null"`
+	CharacterID  uint  `gorm:"unique"`
+}
+
+type CreateAttribute struct {
+	Strength     uint8 `json:"strength" query:"strength" form:"strength"`
+	Dexterity    uint8 `json:"dexterity" query:"dexterity" form:"dexterity"`
+	Constitution uint8 `json:"constitution" query:"constitution" form:"constitution"`
+	Intelligence uint8 `json:"intelligence" query:"intelligence" form:"intelligence"`
+	Wisdom       uint8 `json:"wisdom" query:"wisdom" form:"wisdom"`
+	Charisma     uint8 `json:"charisma" query:"charisma" form:"charisma"`
+	CharacterID  uint
+}
+
+type UpdateAttribute struct {
+	Strength     uint8 `json:"strength" query:"strength" form:"strength"`
+	Dexterity    uint8 `json:"dexterity" query:"dexterity" form:"dexterity"`
+	Constitution uint8 `json:"constitution" query:"constitution" form:"constitution"`
+	Intelligence uint8 `json:"intelligence" query:"intelligence" form:"intelligence"`
+	Wisdom       uint8 `json:"wisdom" query:"wisdom" form:"wisdom"`
+	Charisma     uint8 `json:"charisma" query:"charisma" form:"charisma"`
+	CharacterID  uint
+}
+
+type AttributeExternal struct {
+	Strength     uint8 `json:"strength" query:"strength" form:"strength"`
+	Dexterity    uint8 `json:"dexterity" query:"dexterity" form:"dexterity"`
+	Constitution uint8 `json:"constitution" query:"constitution" form:"constitution"`
+	Intelligence uint8 `json:"intelligence" query:"intelligence" form:"intelligence"`
+	Wisdom       uint8 `json:"wisdom" query:"wisdom" form:"wisdom"`
+	Charisma     uint8 `json:"charisma" query:"charisma" form:"charisma"`
+	CharacterID  uint
 }
